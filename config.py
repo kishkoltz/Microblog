986d9435e1e83f690e04e9ece10ae100c2dd5d88
kishkoltz <krzysztof.filimonowicz@gmail.com>
2018-06-11 15:27:07 +0200

    Flask Database: Setting up SQLite

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -13,3 +14,6 @@ class Config(object):
     and signatures generated with it depends on no person outside
     of the trusted maintainers of the application knowing it.
     '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
