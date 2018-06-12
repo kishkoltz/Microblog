c38d77ada90954f035ea21e512e96e125b5bd14a
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-06-13 00:44:18 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username= db.Column(db.String(64), index=True,unique=True)
