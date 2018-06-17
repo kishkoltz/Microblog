3c717c5d2778716adbb3e873258fdff0e17eb5a4
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-06-17 12:12:04 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -63,7 +63,7 @@ def register():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
-        {'autor': user, 'body': 'Test post #1'},
-        {'autor': user, 'body': 'Test post #2'}
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
