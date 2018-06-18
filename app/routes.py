4bbd2e294621a86f07d17ea2dc9ed35259e3456d
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-06-18 06:12:09 +0200

    Record time of last visit

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -21,6 +22,12 @@ def index():
     ]
     return render_template('index.html',
     title = 'Home', posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
