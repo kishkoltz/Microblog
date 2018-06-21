38bda789b51c919125c79875a72e9d92469646ff
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-06-21 05:14:10 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-import logging
+import logging, os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -28,5 +28,14 @@ if not app.debug:
       credentials=auth, secure=secure)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+  if not os.path.exists('logs'):
+    os.mkdir('logs')
+  file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                    backupCount=10)
+  file_handler.setLevel(logging.INFO)
+  app.logger.addHandler(file_handler)
+  
+  app.logger.setLevel(logging.INFO)
+  app.logger.info('Microblog startup')
 
 from app import routes, models, errors
