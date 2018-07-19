6cc4ee585a393206bc653f55e3958470c0af75b8
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-07-20 01:08:58 +0200

    Posts per page configuration

--- config.py
+++ config.py
@@ -23,3 +23,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['krzysztof.filimonowicz@gmail.com']
+    POSTS_PER_PAGE = 3
