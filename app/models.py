ab9f9f0dec6a51556d03162c8522367401454265
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-08-27 14:45:40 +0200

    Reset password token methods

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from app import db, login
+from app import db, login, app
+from time import time
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+import jwt
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -52,6 +54,19 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                           algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
