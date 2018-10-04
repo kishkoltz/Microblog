99cbd5230ebe306ba337a4fb00af70a8583425a8
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-10-04 12:27:17 +0200

    Select best language

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import logging, os
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -45,5 +45,9 @@ if not app.debug:
   
   app.logger.setLevel(logging.INFO)
   app.logger.info('Microblog startup')
+  
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import routes, models, errors
