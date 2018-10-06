a2aa2c73bb7df4d71a6d5be4ad0f480e34a97ecd
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-10-06 04:49:46 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
-from flask_babel import _, lazy_gettext as _l
+from flask_babel import _ 
+from flask_babel import lazy_gettext as _l
 from app.models import User
 
 class LoginForm(FlaskForm):
