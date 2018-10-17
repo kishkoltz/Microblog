43cdd3f9f64045e9c73fc3cf9248cd44002e7d60
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-10-17 10:43:26 +0200

    Text translation view function

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordForm, ResetPasswordRequestForm
 from app.email import send_password_reset_email
@@ -8,6 +8,7 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 from flask_babel import _, get_locale
 from guess_language import guess_language
+from app.translate import translate
 
 @app.before_request
 def before_request():
@@ -182,3 +183,10 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form = form)
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
