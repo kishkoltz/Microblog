0f237e90e139af851327ebeb0065803d74394e1f
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2018-10-23 18:57:41 +0200

    Update translate.py

--- app/translate.py
+++ app/translate.py
@@ -1,4 +1,5 @@
 from yandex_translate import YandexTranslate
+from flask import current_app
 from flask_babel import _
 import json
 from app import app
@@ -7,10 +8,10 @@ def translate(text, source_language, dest_language):
   # Yandex doesn't require requests (implied?)
   # Yandex doesn't require wrapping key in auth
   # Yandex returns a dict of lists, the translation is found in ['text'][0]
-    if 'TRANSLATOR_KEY' not in app.config or \
-        not app.config['TRANSLATOR_KEY']:
+    if 'TRANSLATOR_KEY' not in current_app.config or \
+        not current_app.config['TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
-    t = YandexTranslate(app.config['TRANSLATOR_KEY'])
+    t = YandexTranslate(current_app.config['TRANSLATOR_KEY'])
     r = t.translate(text, source_language + '-' + dest_language)
     if r['code'] != 200:
         return "Error: the translation service failed. %r" % ( r['code'] )
