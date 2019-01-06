97c43eb96eb59109113f1e82326b34ee9a5893ff
kishkoltz <37917156+kishkoltz@users.noreply.github.com>
2019-01-06 03:51:20 +0100

    Update search.py

--- app/search.py
+++ app/search.py
@@ -7,10 +7,6 @@ ix = index.open_dir("index")
 
 
 def add_to_index(model):
-    #body = __searchable__
-    #payload = {}
-    #for field in model.__searchable__:
-    #    payload[field] = getattr(model, field)
     writer = ix.writer()
     writer.add_document(id=str(model.id), body=model.body)
     writer.commit()
@@ -19,21 +15,5 @@ def query_index(query, page, per_page):
     q = QueryParser('body', ix.schema).parse(query)
     with ix.searcher() as searcher:
         results = searcher.search_page(q, page, pagelen=per_page)
-        
-        #ids = [int(hit['_id']) for hit in search['hits']['hits']]
-    #return ids, search['hits']['total']
         ids = [int(result['id']) for result in results[:]]
         return ids, len(results)
-        #for result in results[:]:
-        #    print(result['id'])
-        #print(results[:], len(results))
-    
-'''    
->>> from app.search import *
->>> for post in Post.query.all():
-...     add_to_index(post)
-... 
->>> query_index("hello", 1, 5)
-[<Hit {'id': '3'}>, <Hit {'id': '4'}>] 2   
-'''    
-#https://whoosh.readthedocs.io/en/latest/searching.html
